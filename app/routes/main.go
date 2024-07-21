addb0474300c9762e2cbf52e5016c14b95abdbca
lrq <miv1kk.1972@gmail.com>
2024-07-22 01:08:38 +0300

    some_inf

--- app/routes/main.go
+++ app/routes/main.go
@@ -8,10 +8,12 @@ import (
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
+	// получаем директорию
 	wd, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
 	}
+	// указываем путь: что хотим отправлять
 	tpl := template.Must(template.ParseFiles(wd + "\\app\\templates\\index.html"))
 
 	tpl.Execute(w, nil)
@@ -26,6 +28,7 @@ func main() {
 
 	mux := http.NewServeMux()
 
+	// обработчик всех статик-файлов (js, css, img и т.д)
 	fs := http.FileServer(http.Dir("app/static"))
 	mux.Handle("/static/", http.StripPrefix("/static/", fs))
 
